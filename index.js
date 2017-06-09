e2000f9a5b5364af1ef398807ae3040f390e34ce
andreas-optimizely <andreas@optimizely.com>
2017-06-09 11:49:40 -0700

    add some interactions

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 'use strict';
-const express = require('express'),
-        ngrok = require('ngrok'),
-       twilio = require('twilio'),
-   bodyParser = require('body-parser'),
-         uuid = require('uuid');
+const         express = require('express'),
+                ngrok = require('ngrok'),
+               twilio = require('twilio'),
+           bodyParser = require('body-parser'),
+                 uuid = require('uuid/v4'),
+           optimizely = require('optimizely-server-sdk'),
+  defaultErrorHandler = require('optimizely-server-sdk/lib/plugins/error_handler'),
+        defaultLogger = require('optimizely-server-sdk/lib/plugins/logger'),
+              request = require('request-promise');
 
 const app = express();
+const projectId = '8430132013';
+const datafileUrl = `https://cdn.optimizely.com/json/${projectId}.json`;
+
+let optimizelyClient;
+
+// Initialize Optimizely client on server start
+request({uri: datafileUrl, json: true}).then((datafile) => { 
+  console.log('Initializing Optimizely Client with Datafile: ', datafile);
+  optimizelyClient = optimizely.createInstance({
+    datafile: datafile,
+    errorHandler: defaultErrorHandler,
+    logger: defaultLogger.createLogger()
+  });
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,16 +31,28 @@ app.get('/', (req, res) => {
   res.send('SMS Full Stack Bot');
 });
 
+// Webhook URL for SMS
 app.post('/sms-webhook', (req,res)=>{
   let msg = req.body.Body.toLowerCase();
-  var twiml = new twilio.twiml.MessagingResponse();
+  let twiml = new twilio.twiml.MessagingResponse();
+  // Generate random user id to simulate random bucketing
+  let userId = uuid();
+  let fromNumber = req.body.From;
+  let variation;
 
-  if(msg === "hello"){
-    twiml.message("Hey there!");
-  } else if (msg.includes('mail') && msg.includes('payment')) {
+  if(msg === "hello" || msg ==='hi' || msg ==='hey'){
+    let response = 'Hey there! Nice to meet you, let\'s get started. Just ask and I\'ll answer';
+    variation = optimizelyClient.activate('introduction_experiment', userId);
+    console.log(variation);
+    variation === "video_demo" ? response += '. Here\'s a quick video to show you what I can do https://youtu.be/jvyHcjZoGJk' : null;
+    twiml.message(response);
+  } else if (msg.includes('mail') || msg.includes('send') && msg.includes('payment')) {
     twiml.message("Looking to mail your credit card payment?");
+  } else {
+    twiml.message('Sorry I didn\'t get that');
   }
-
+  optimizely.track("sent_msg", userId);
+  
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
