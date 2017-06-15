dcebef5fa810d2b675962818a27448cd303f8f01
andreas-optimizely <andreas@optimizely.com>
2017-06-14 18:10:19 -0700

    add start script

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 const         express = require('express'),
-                ngrok = require('ngrok'),
                twilio = require('twilio'),
            bodyParser = require('body-parser'),
                  uuid = require('uuid/v4'),
@@ -35,24 +34,42 @@ app.get('/', (req, res) => {
 app.post('/sms-webhook', (req,res)=>{
   let msg = req.body.Body.toLowerCase();
   let twiml = new twilio.twiml.MessagingResponse();
+
   // Generate random user id to simulate random bucketing
   let userId = uuid();
   let fromNumber = req.body.From;
   let variation;
 
   if(msg === "hello" || msg ==='hi' || msg ==='hey'){
+    
     let response = 'Hey there! Nice to meet you, let\'s get started. Just ask and I\'ll answer';
     variation = optimizelyClient.activate('introduction_experiment', userId);
+    
     console.log(variation);
+
     variation === "video_demo" ? response += '. Here\'s a quick video to show you what I can do https://youtu.be/jvyHcjZoGJk' : null;
     twiml.message(response);
+
   } else if (msg.includes('mail') || msg.includes('send') && msg.includes('payment')) {
-    twiml.message("Looking to mail your credit card payment?");
+    let response = "Since you're in, " + req.body.ToCity + " here's the closest place to mail your payment, 123 Main St, San Francisco, CA 94105";
+    variation = optimizelyClient.activate('mail_experiment', userId);
+    console.log(variation);
+    
+    if (variation === "app"){
+      response = 'Instead of mailing your payment, you can go paperless and save the postage by paying via our app. http://apple.co/1HQ8ikO';
+    }
+
+    else if(variation === "eno"){
+      response = 'Did you know I can also make payments for you? Just tell me which card and how much';
+    }
+
+    twiml.message(response);
   } else {
     twiml.message('Sorry I didn\'t get that');
   }
-  optimizely.track("sent_msg", userId);
-  
+
+  optimizelyClient.track("sent_msg", userId);
+
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
